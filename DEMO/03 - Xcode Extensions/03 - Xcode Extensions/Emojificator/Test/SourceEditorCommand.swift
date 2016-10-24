75a5593fedc7cf4d414fa8411b626ab4310a81d9
LeeJay <kengsir@KengsirdeMacBook-Pro.local>
2016-10-24 19:20:58 +0800

    iOS day4

---
+++ DEMO/03 - Xcode Extensions/03 - Xcode Extensions/Emojificator/Test/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  Test
+//
+//  Created by Kengsir on 2016/10/13.
+//  Copyright © 2016年 ShinobiControls. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
